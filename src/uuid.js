d8a28b0b596e438b4ec979b5553e546419e04a70
SamJakob <me@samjakob.com>
2020-03-27 00:07:12 +0000

    Create uuid.js

---
+++ src/uuid.js
@@ -0,0 +1,10 @@
+export default {
+  
+  v4 () {
+    // From https://gist.github.com/jed/982883
+    return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+      (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+    );
+  }
+  
+};
